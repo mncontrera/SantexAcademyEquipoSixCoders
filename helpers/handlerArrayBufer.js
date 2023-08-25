758a15ec634be4522f0c7894fd282d4ab8a302dc
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-08-25 10:26:48 -0300

    primer commit
    conversion a bytes

---
+++ helpers/handlerArrayBufer.js
@@ -0,0 +1,11 @@
+function arrayBufferToBase64(buffer) {
+  let binary = '';
+  const bytes = new Uint8Array(buffer);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i + 1) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
+
+module.exports = { arrayBufferToBase64 };
--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const db = require('../models');
+const { arrayBufferToBase64 } = require('../helpers/handlerArrayBufer');
 
 const saltRound = 10;
 
@@ -24,43 +25,45 @@ async function login(email, password) {
   });
 
   if (!user) {
-    throw new Error(JSON.stringify('usuario no encontrado'));
+    throw new Error('usuario no encontrado');
   }
   const checkPassword = await bcrypt.compare(password, user.password);
   if (!checkPassword) {
-    throw new Error(JSON.stringify('Contrasena incorrecta'));
+    throw new Error('Contrasena incorrecta');
   }
   const token = jwt.sign({
     id: user.id,
     name: user.name,
   }, 'claveSixCoders');
 
+  const imageByteArray = new Uint8Array(user.image);
+  const base64Image = arrayBufferToBase64(imageByteArray);
+
   return {
-    accesToken: token,
-    user: db.User.name,
+    accessToken: token,
+    user: {
+      id: user.id,
+      name: user.name,
+      image: `data:image/jpeg;base64,${base64Image}`,
+    },
   };
 }
 
 async function edit(id, name, lastname, email, password, rolId, image) {
-  // const passwordHash = await bcrypt.hash(password, saltRound);
-
+  const passwordHash = await bcrypt.hash(password, saltRound);
   const user = await db.User.findByPk(id);
+
   if (!user) {
     throw new Error(JSON.stringify('Usuario no encontrado'));
   }
-  const updatedFields = {};
-
-  updatedFields.name = name;
-
-  updatedFields.lastname = lastname;
-
-  updatedFields.email = email;
-
-  updatedFields.password = password;
-
-  updatedFields.rolId = rolId;
-
-  updatedFields.image = image;
+  const updatedFields = {
+    name,
+    lastname,
+    email,
+    password: passwordHash,
+    rolId,
+    image,
+  };
   await user.update(updatedFields);
   return user;
 }
