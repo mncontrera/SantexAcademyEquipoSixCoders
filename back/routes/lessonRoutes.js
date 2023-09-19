f6505e6544642fa6612654bc0501d05cdb2374f4
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-09-19 12:29:01 -0300

    manejo-de-asistencias

--- back/routes/lessonRoutes.js
+++ back/routes/lessonRoutes.js
@@ -5,5 +5,9 @@ const lessonController = require('../controllers/lessonController');
 const router = express.Router();
 
 router.post('/create', lessonController.createLesson);
+router.get('/getLesson', lessonController.getLesson);
+router.get('/getAllLessons', lessonController.getAllLessons);
+router.put('/editLesson', lessonController.editLesson);
+router.delete('/deleteLesson', lessonController.deleteLesson);
 
 module.exports = router;
