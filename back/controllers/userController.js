1c0b136e5155d829e51930c8422fb1080974f4fe
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-10-06 11:33:31 -0300

    funcionamiento a revisar

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -98,6 +98,18 @@ async function userProfile(req, res, next) {
   }
 }
 
+async function sendEmail(req, res) {
+  const { correo, asunto, description } = req.body;
+
+  try {
+    await userService.sendEmail(correo, asunto, description);
+    return res.status(200).json({ message: 'Correo electrónico enviado con éxito' });
+  } catch (error) {
+    console.error('Error al enviar el correo electrónico:', error);
+    return res.status(500).json({ error: 'Error al enviar el correo electrónico' });
+  }
+}
+
 module.exports = {
-  createUser, loginUser, editUser, deleteUser, userProfile,
+  createUser, loginUser, editUser, deleteUser, userProfile, sendEmail,
 };
