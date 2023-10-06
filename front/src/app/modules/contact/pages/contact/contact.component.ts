22e3dc698c02eebb10f2f2d4b562c226e741561c
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-10-06 17:11:46 -0300

    sendEmail

--- front/src/app/modules/contact/pages/contact/contact.component.ts
+++ front/src/app/modules/contact/pages/contact/contact.component.ts
@@ -11,19 +11,6 @@ import { ApiService } from 'src/app/core/http/api.service';
 })
 export class ContactComponent implements OnInit {
 
-  contactForm: UntypedFormGroup;
-  elemento:any;
-
-  constructor(
-    private uFormBuilder: UntypedFormBuilder,
-    private router: Router,
-    private apiService: ApiService,
-    private httpClient: HttpClient
-  ) {
-    this.contactForm = this.uFormBuilder.group({
-      correo: [``,[Validators.required, Validators.maxLength(150)]],
-      asunto: [``,[Validators.required, Validators.maxLength(150)]],
-      description: [``,[Validators.required, Validators.maxLength(300),]]
     })
    }
 
@@ -31,20 +18,3 @@ export class ContactComponent implements OnInit {
    
   }
 
-  sendConsult($event: any) {
-    if(this.contactForm.valid) {
-      console.log(this.contactForm.value);
-    }else{
-      this.contactForm.markAllAsTouched();
-    }
-  }
-
-  get titleField(){
-    return this.contactForm.get('correo');
-  }
-
-  get descriptionField(){
-    return this.contactForm.get('description');
-  }
-
-}
